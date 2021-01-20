62b8d80e2a450005e3b0b838aa0a7922d2858a07
Merve Bozkurt <bbozkurtmerve@gmail.com>
2021-01-21 00:12:11 +0300

    20 Ocak ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //Constructor newlediğin an çalışan sınıf constructor
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {   //yeni eleman gelince önceki referasın numarası uçmasın diye tempe atıyoruz eskiyi
+            T[] tempArray = items;
+            items = new T[items.Length + 1];//eleman sayısını bir arttırdım.
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i]; //tempArraya geçici attığımız elemanları yeniden items arrayına  atıyoruz
+
+            }
+            items[items.Length - 1] = item; //eklenmek istenen eleman eklendi.
+
+        }
+        
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 5, 
+                ProductName = "Kalem", UnitPrice = 35 };
+
+            //PascalCase //camelCase
+            //case sensitive
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+            int sayi = 100;
+            productManager.BiseyYap(sayi); //değişiklik buraya yansımadı method da yapılan.
+            Console.WriteLine(sayi  ); //100 
+            //ref out
+        }
+    }
+}
